233f849065d9246743499169f11c9884f802057e
unknown <hidan818118@gmail.com>
2022-02-18 21:26:32 +0700

    dsfF

---
+++ src/B1_Intro_to_Java/Practive/DeclareAndUse.java
@@ -0,0 +1,18 @@
+package B1_Intro_to_Java.Practive;
+
+public class DeclareAndUse {
+    public static void main(String[] args) {
+        int i = 2;
+        float f = 2f;
+        double d = 5.9;
+        boolean b = false;
+        char c = 'b';
+        String s = "Chào";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
---
+++ src/B1_Intro_to_Java/Practive/ShowTime.java
@@ -0,0 +1,10 @@
+package B1_Intro_to_Java.Practive;
+
+import java.util.Date;
+
+public class ShowTime {
+    public static void main(String[] args) {
+    Date now = new Date();
+        System.out.println("Now is: " +now );
+    }
+}
